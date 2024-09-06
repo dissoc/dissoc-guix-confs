d7ae56133084e31c4a8c9f6294cfe30b4878cab4
Justin Bishop <mail@dissoc.me>
2024-09-06 16:15:32 -0500

    added nftables portknocking rules

--- dissoc/confs/nftables/portknocking.scm
+++ dissoc/confs/nftables/portknocking.scm
@@ -1,33 +1,33 @@
-;; (define-module (dissoc confs nftables portknocking))
+(define-module (dissoc confs nftables portknocking))
 
-;; (define-public %ntables-portknocking
-;;   "
-;; # Port knocking rules
-;; define guarded_ports = {ssh}
+(define-public %ntables-portknocking
+  "
+# Port knocking rules
+define guarded_ports = {ssh}
 
-;; table inet portknock {
+table inet portknock {
 
-;;   set clients_ipv4 {
-;;     type ipv4_addr
-;;     flags timeout
-;;   }
+  set clients_ipv4 {
+    type ipv4_addr
+    flags timeout
+  }
 
-;;   set candidates_ipv4 {
-;;     type ipv4_addr . inet_service
-;;     flags timeout
-;;   }
+  set candidates_ipv4 {
+    type ipv4_addr . inet_service
+    flags timeout
+  }
 
-;;   chain input {
-;;     type filter hook input priority -10; policy accept;
+  chain input {
+    type filter hook input priority -10; policy accept;
 
-;;     iifname \"lo\" return
+    iifname \"lo\" return
 
-;;     tcp dport 55555 add @candidates_ipv4 {ip  saddr . 4444 timeout 30s}
-;;     tcp dport 4444 ip  saddr . tcp dport @candidates_ipv4 add @candidates_ipv4 {ip  saddr . 333 timeout 30s}
-;;     tcp dport 333 ip  saddr . tcp dport @candidates_ipv4 add @candidates_ipv4 {ip  saddr . 54321 timeout 30s}
-;;     tcp dport 54321 ip  saddr . tcp dport @candidates_ipv4 add @clients_ipv4 {ip  saddr timeout 30s} log prefix \"Successful portknock: \"
-;;     tcp dport $guarded_ports ip  saddr @clients_ipv4 counter accept
-;;     tcp dport $guarded_ports ct state established,related counter accept
-;;     tcp dport $guarded_ports counter reject with tcp reset
-;;   }
-;; }")
+    tcp dport 55555 add @candidates_ipv4 {ip  saddr . 4444 timeout 30s}
+    tcp dport 4444 ip  saddr . tcp dport @candidates_ipv4 add @candidates_ipv4 {ip  saddr . 333 timeout 30s}
+    tcp dport 333 ip  saddr . tcp dport @candidates_ipv4 add @candidates_ipv4 {ip  saddr . 54321 timeout 30s}
+    tcp dport 54321 ip  saddr . tcp dport @candidates_ipv4 add @clients_ipv4 {ip  saddr timeout 30s} log prefix \"Successful portknock: \"
+    tcp dport $guarded_ports ip  saddr @clients_ipv4 counter accept
+    tcp dport $guarded_ports ct state established,related counter accept
+    tcp dport $guarded_ports counter reject with tcp reset
+  }
+}")
