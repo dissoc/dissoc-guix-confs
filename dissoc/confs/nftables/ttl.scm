a966e8ffc97edb35e710455b832677856f73494e
Justin Bishop <mail@dissoc.me>
2025-07-02 20:42:32 -0500

    added the fn input to set the ttl

--- dissoc/confs/nftables/ttl.scm
+++ dissoc/confs/nftables/ttl.scm
@@ -1,16 +1,18 @@
 (define-module (dissoc confs nftables ttl))
 
-(define-public (generate-ttl-table interface-name)
+;; If you're here then you probably want to set ttl to 66 (65+1)
+;; or 65 + n, where n is the number of routes
+(define-public (generate-ttl-table interface-name ttl)
   (string-append "table inet tether_ttl {
 	chain postrouting {
 		type filter hook postrouting priority srcnat; policy accept;
-		oifname " interface-name " ip ttl set 66
-		oifname " interface-name " ip6 hoplimit set 66
+		oifname " interface-name " ip ttl set " ttl "
+		oifname " interface-name " ip6 hoplimit set " ttl "
 	}
 
 	chain prerouting {
 		type filter hook prerouting priority 100; policy accept;
-		iifname " interface-name " ip ttl set 66
-		iifname " interface-name " ip6 hoplimit set 66
+		iifname " interface-name " ip ttl set " ttl "
+		iifname " interface-name " ip6 hoplimit set " ttl "
 	}
 }"))
